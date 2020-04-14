c619c1b54717aa35eee1b490f8c120f1c15341f6
code <xin@daotech.io>
2020-04-14 17:29:30 +0800

    添加父子组件通信

--- src/views/Communication/ParentAndChild/ParentAndChild.vue
+++ src/views/Communication/ParentAndChild/ParentAndChild.vue
@@ -1,15 +1,43 @@
 <template>
-<div id="parentAndChild">
-
+<div id="parentAndChild" ref='parent'>
+  <h3>我是父组件</h3>
+  <button class="ant-btn" @click='childHandle'>通过refs操作子组件</button>
+  <hr />
+  <child :apple='apple' @decreasehandle='decreasehandle' @addhandle='addhandle' ref='child' />
 </div>
 </template>
 <script>
+import child from './child.vue';
 export default {
   name: "ParentAndChild",
+  components: {
+    child
+  },
   data: () => ({
+    apple: 5,
+    banana: 6
+  }),
+  methods: {
+    decreasehandle() {
+      this.apple--;
+    },
+    addhandle() {
+      this.apple++;
+    },
+    childHandle() {
+      let child = this.$refs.child;
+      child.addHandle()
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
 
-  })
+    })
+  }
 }
 </script>
 <style lang="scss" scoped>
+button {
+    border: 1px solid #e8e8e8;
+}
 </style>
---
+++ src/views/Communication/ParentAndChild/child.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="child">
+  <h5>我是子组件</h5>
+  <p>{{apple}}</p>
+  <button class="ant-btn" @click='lessHandle'>通过props和$emit操作</button>
+</div>
+</template>
+<script>
+export default {
+  name: "child",
+  props: ['apple'],
+  data: () => ({
+    isClick: false,
+  }),
+  methods: {
+    lessHandle() {
+      this.isClick = true;
+      this.$emit('decreasehandle')
+    },
+    addHandle() {
+      this.$emit('addhandle')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
